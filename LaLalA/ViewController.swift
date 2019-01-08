e31dd06c2a29eb6155b38e9117a2bd876ca975d4
Duc <ducban@admins-imac-3.local>
2019-01-08 09:18:23 +0700

    Initial Commit

---
+++ LaLalA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LaLalA
+//
+//  Created by Duc on 08/01/2019.
+//  Copyright © 2019 Duc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
